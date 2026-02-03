c295cba3c8423b1d7bfca4a79e39feec5656cd1e
Gregory Lefebvre <gregory.lfb1@gmail.com>
2026-02-03 15:52:07 +0100

    redis cache store config fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,12 +68,11 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Use Redis for caching in production
-  config.cache_store = :redis_cache_store, {
+  config.cache_store = :redis_cache_store,
     url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'),
     namespace: 'nexp_cache',
     expires_in: 90.minutes,
     reconnect_attempts: 3
-  }
 
   # Use Sidekiq for background jobs
   config.active_job.queue_adapter = :sidekiq
