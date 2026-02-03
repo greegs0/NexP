800c178e3b832f57d143c21b3c61c4ffc10f10cf
Grégory Lefebvre <gregory.lfb1@gmail.com>
2026-02-03 17:04:06 +0100

    Fix Redis SSL for Heroku (self-signed cert)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,16 @@
+redis_config = {
+  url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1')
+}
+
+# Heroku Redis uses self-signed certificates
+if ENV['REDIS_URL']&.start_with?('rediss://')
+  redis_config[:ssl_params] = { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+end
+
 Sidekiq.configure_server do |config|
-  config.redis = {
-    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1')
-  }
+  config.redis = redis_config
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1')
-  }
+  config.redis = redis_config
 end
